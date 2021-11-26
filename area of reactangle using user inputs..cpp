6064025d516a56798f4f992a5b5466aa4d9a381c
NarendraBhavsar99 <95095281+NarendraBhavsar99@users.noreply.github.com>
2021-11-26 22:57:49 +0530

    Add files via upload

---
+++ area of reactangle using user inputs..cpp
@@ -0,0 +1,16 @@
+//area of reactangle using user inputs.
+
+#include<stdio.h>
+
+int main(){
+	int l,b;
+	
+	printf("enter the value of l\n");
+	scanf("%d", &l);
+	printf("enter the value of b\n");
+	scanf("%d", &b);
+	
+	printf("area of reactangle is %d", l*b);
+	
+	return 0;	
+}
